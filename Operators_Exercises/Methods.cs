f4f84bd44f5755eae2cd707a5b18dcc707cf6d9f
John Ward <johndward01@gmail.com>
2022-09-14 13:39:18 -0500

    Add project files.

---
+++ Operators_Exercises/Methods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators_Exercises;
+internal class Methods
+{
+    public static double AreaOfCircle(double radius)
+    {
+        return Math.PI * Math.Pow(radius, 2);
+    }
+}
---
+++ Operators_Exercises/Program.cs
@@ -0,0 +1,43 @@
+﻿using Operators_Exercises;
+
+
+//int x = 10;
+//int y = 11;
+
+//x += y; // output 21
+//x -= y; // 10
+//x *= y; // 110
+//x %= y; // 0
+
+// Exercise 1
+
+//int a = 17;
+//int b = 4;
+
+//int quotient = a / b;
+//int remainder = a % b;
+
+
+//Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+
+// Exercise 2
+Console.WriteLine("Please enter the radius: ");
+
+while (true)
+{
+    try
+    {
+        double radius = Convert.ToDouble(Console.ReadLine());
+        double area = Methods.AreaOfCircle(radius);
+        Console.WriteLine(area);
+        break;
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine(e.Message);
+        Console.WriteLine("We encountered an error but it didn't crash the program");
+    }
+}
+
+
